b1bcbe5882f8f2b5f99bb00c8a2cb0790de78cb8
wgreine2 <144729039+Will-GreinerUWYO@users.noreply.github.com>
2024-11-27 21:48:12 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,35 @@
 function augmentingPath(graph, start, end) {
+    let path = [];
+    if(start === end) {
+        return start;
+    }
+    let toCheck = [];
+    let nodeCount = 0;
+
+    for(let i = 0; i < graph.length; i++){
+        toCheck[0] = graph[i][0];
+        nodeCount++;
+    }
+    
+    path[0] = start;
+    let currentNode = start;
+    let nextNode = null;
+    let nextNodeVal = 0;
+    
+    while(nodeCount > 0){
+        if(currentNode === end){
+            return path;
+        }
+        for(connected[0] in graph[currentNode][connected][0]){
+            if (connected[1] > nextNodeVal){
+                nextNode = connected;
+                nextNodeVal = connected[1];
+            }
+        }
+        path.push(nextNode);
+        currentNode = nextNode;
+        nextNodeVal = 0;
+        nodeCount--;
+    }
     return [];
 }
